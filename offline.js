8d7688b4ce4ab513dcff313510b6fe69da816aa3
Luiz Carlos <frickajr@gmail.com>
2020-05-22 09:45:47 -0300

    Deploy inicia

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1590150616,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/aimspot-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/peerlaser-sheet0.png",
+		"images/bar-sheet0.png",
+		"images/floorbar.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
